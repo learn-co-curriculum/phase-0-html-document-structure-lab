b3ceadc62f4c2aa25bd6c53b5585e9f6e2df64a1
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-09-27 16:24:21 -0400

    Removes confusing, unnecessary sections/tests

--- README.md
+++ README.md
@@ -93,44 +93,10 @@ The `head` section is not visible to a website visitor, but it contains a lot of
 useful info about our webpage.
 
 In the `head` section, we place a number of specific tags, most notably:
-  - `<meta>`
   - `<link>`
   - `<title>`
 
 Let's look at each of them in turn:
-
-### `meta`
-
-The `meta` tag provides metadata about the document, including what character
-set to use, a description of the content, specific keywords, and the author.
-
-Adding this metadata on the content of the page helps search engines know what
-the page contains, so taking the time to include these will help bump your web
-pages up in rank on relevant searches.
-
-There is also a `viewport` method, which instructs the browser on how to control
-the page's dimensions and scaling. This becomes important when adding styling,
-as some mobile devices automatically scale web pages if there is not specific
-setting provided.
-
-Examples of the `<meta>` tags types we can add into our `head` section:
-
-```html
-<!-- UTF-8 is the standard for HTML5 web pages -->
-<meta charset="UTF-8">
-
-<!-- This description will appear on search engines, displayed as the summary under a web page's link -->
-<meta name="description" content="A website devoted to pictures of all my favorite cats, kittens and felines">
-
-<!-- Keywords, separated by commas, help search engine spiders in understanding page content -->
-<meta name="keywords" content="cats, kittens, felines, lions, tigers">
-
-<!-- Take credit for your work by including this author meta tag on pages you create -->
-<meta name="author" content="Cat Perry">
-
-<!-- Setting the viewport like this will ensure mobile devices don't scale content -->
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-```
 
 ### `link`
 
@@ -159,6 +125,7 @@ might see a series of `<link>` definitions like:
 <link rel="stylesheet" type="text/css" href="engineering-department.css">
 <link rel="stylesheet" type="text/css" href="project-x-launch.css">
 <link rel="stylesheet" type="text/css" href="typography.css">
+```
 
 In this example we're getting some CSS information from the
 [Bootstrap](https://getbootstrap.com/) project, we're integrating a company
--- spec/document_structure_spec.rb
+++ spec/document_structure_spec.rb
@@ -31,64 +31,6 @@ RSpec.describe "Well-Formed HTML Document" do
   end
 
   context 'within <head>' do
-    it 'contains a <meta> tag for the character set' do
-      head = parsed_html.search('html > head')
-      meta = head.children.select {|ch|
-        ch.name == "meta" &&
-        ch.attributes["charset"]
-      }.first
-      expect(meta).to_not be_nil, "No <meta> tag with a 'charset' attribute found"
-      expect(meta.attributes["charset"].value).to eq("UTF-8"), "The 'charset' attribute does not equal 'UTF-8'"
-    end
-
-    it 'contains a <meta> tag for the description' do
-      head = parsed_html.search('html > head')
-      meta = head.children.select {|ch|
-        ch.name == "meta" &&
-        ch.attributes["name"] &&
-        ch.attributes["name"].value == "description"
-      }.first
-      expect(meta).to_not be_nil, "No <meta> tag with a 'name' attribute set to 'description' was found"
-      expect(meta.attributes["content"]).to_not be_nil, "No 'content' attribute found for the description meta tag"
-      expect(meta.attributes["content"].value).to_not eq(""), "The content attribute needs a text value"
-    end
-
-    it 'contains a <meta> tag for keywords' do
-      head = parsed_html.search('html > head')
-      meta = head.children.select {|ch|
-        ch.name == "meta" &&
-        ch.attributes["name"] &&
-        ch.attributes["name"].value == "keywords"
-      }.first
-      expect(meta).to_not be_nil, "No <meta> tag with a 'name' attribute set to 'keywords' was found"
-      expect(meta.attributes["content"]).to_not be_nil, "No 'content' attribute found for the keywords meta tag"
-      expect(meta.attributes["content"].value).to_not eq(""), "The content attribute needs a text value"
-    end
-
-    it 'contains a <meta> tag for the author' do
-      head = parsed_html.search('html > head')
-      meta = head.children.select {|ch|
-        ch.name == "meta" &&
-        ch.attributes["name"] &&
-        ch.attributes["name"].value == "author"
-      }.first
-      expect(meta).to_not be_nil, "No <meta> tag with a 'name' attribute set to 'author' was found"
-      expect(meta.attributes["content"]).to_not be_nil, "No 'content' attribute found for the author meta tag"
-      expect(meta.attributes["content"].value).to_not eq(""), "The content attribute needs a text value"
-    end
-
-    it 'contains a <meta> tag for the viewport' do
-      head = parsed_html.search('html > head')
-      meta = head.children.select {|ch|
-        ch.name == "meta" &&
-        ch.attributes["name"] &&
-        ch.attributes["name"].value == "viewport"
-      }.first
-      expect(meta).to_not be_nil, "No <meta> tag with a 'name' attribute set to 'viewport' was found"
-      expect(meta.attributes["content"]).to_not be_nil, "No 'content' attribute found for the viewport meta tag"
-      expect(meta.attributes["content"].value).to eq("width=device-width, initial-scale=1.0"), "The content attribute for the viewport meta tag should equal 'width=device-width, initial-scale=1.0'"
-    end
-
     it 'contains a <link> tag to an external stylesheet' do
       link = parsed_html.search('html > head > link').first
       expect(link).to_not be_nil, "No <link> tag found in the <head>"
