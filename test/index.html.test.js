412d733b681cceee8694877f4d7349a1be03708b
ihollander <ianholla@gmail.com>
2021-11-16 09:22:32 -0500

    Added chai-dom

--- test/index.html.test.js
+++ test/index.html.test.js
@@ -1,8 +1,11 @@
-const { expect } = require("chai");
+const chai = require("chai");
 const fs = require("fs");
 const path = require("path");
 const globalJsdom = require("global-jsdom");
 const { HtmlValidate } = require("html-validate");
+
+chai.use(require("chai-dom"));
+const { expect } = chai;
 
 const html = fs.readFileSync(
   path.resolve(__dirname, "..", "index.html"),
@@ -13,62 +16,69 @@ globalJsdom(html);
 
 describe("index.html", () => {
   describe("valid document structure", () => {
-    it("begins with a DOCTYPE tag", () => {
-      expect(html, "Missing DOCTYPE html tag").to.contain("<!DOCTYPE html>");
+    it("has a DOCTYPE tag", () => {
+      expect(html).to.contain("<!DOCTYPE html>");
     });
 
     it("has opening and closing HTML tags", () => {
       expect(html).to.contain("<html");
       expect(html).to.contain("</html>");
+
+      // document.querySelector("html") finds the first <html> tag in the HTML document
+      const htmlElement = document.querySelector("html");
+      const hint = "Missing <html> tag";
+
+      expect(htmlElement, hint).to.exist;
     });
 
     it("has a language attribute in the <html> tag", () => {
-      // This finds the first <html> tag in the HTML document
-      const parsedHtml = document.querySelector("html");
-      expect(
-        parsedHtml.lang,
-        'Include a lang attribute assigned to "en" for English'
-      ).to.eq("en");
+      const htmlElement = document.querySelector("html");
+      expect(htmlElement).to.have.attribute("lang", "en");
     });
 
     it("has <head> and <body> tags nested in the <html> tag", () => {
-      expect(html, "Include a <head> tag within the <html> tag").to.contain(
-        "<head>"
-      );
-      expect(html).to.contain("</head>", "Include a closing </head> tag");
-      expect(html).to.contain(
-        "<body>",
-        "Include a <body> tag within the <html> tag"
-      );
-      expect(html).to.contain("</body>", "Include a closing </body> tag");
+      expect(html).to.contain("<head>");
+      expect(html).to.contain("</head>");
+      expect(html).to.contain("<body>");
+      expect(html).to.contain("</body>");
+
       // This finds the first <html> tag in the HTML document
-      const parsedHtml = document.querySelector("html");
+      const htmlElement = document.querySelector("html");
       // parsedHtml.children returns all tags nested within the <html> tag
+
       // the first child should be a <head> tag
-      expect(parsedHtml.children[0].tagName).to.eq("HEAD");
+      const hint1 = "Include a <head> element nested inside the <html> element";
+      expect(htmlElement, hint1).to.have.descendant("head");
+
       // the second child should be a <body> tag
-      expect(parsedHtml.children[1].tagName).to.eq("BODY");
+      const hint2 = "Include a <body> element nested inside the <html> element";
+      expect(htmlElement, hint2).to.have.descendant("body");
     });
   });
 
   describe("valid <head> structure", () => {
-    it("has a <link> tag to an external stylesheet", () => {
+    it("has a <link> tag that links to an external stylesheet", () => {
       // find a <link> tag within the <head> tag
       const link = document.querySelector("head > link");
 
-      expect(link, "No <link> tag found in the <head>").to.exist;
-      expect(link.href, "The 'href' attribute is missing in the link tag").to
-        .exist;
-      expect(link.rel, "The 'rel' attribute is missing in the link tag").to
-        .exist;
+      const hint1 = "No <link> tag found in the <head>";
+      expect(link, hint1).to.exist;
+
+      expect(link).to.have.attribute("href", "style.css");
+
+      expect(link).to.have.attribute("rel", "stylesheet");
     });
 
     it("has a <title> tag to enclose the site title", () => {
       // find a <title> tag within the <head> tag
       const title = document.querySelector("head > title");
 
-      expect(title, "No <title> tag found in the <head>").to.exist;
-      expect(title.textContent).to.eq("My Site Title");
+      const hint1 = "No <title> tag found in the <head>";
+      expect(title, hint1).to.exist;
+
+      const hint2 =
+        "The content inside the <title> tag should be 'My Site Title'";
+      expect(title, hint2).to.have.text("My Site Title");
     });
   });
 
